b4eafea3998c982dc09ed341679671dd58b7a0ee
雷文勇 <35560582+aierx@users.noreply.github.com>
2024-06-13 11:29:05 +0800

    java config

---
+++ vscode/java.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+code-server --uninstall-extension formulahendry.code-runner
+code-server --uninstall-extension github.github-vscode-theme
+code-server --uninstall-extension vscjava.vscode-spring-initializr
+code-server --uninstall-extension vscode-icons-team.vscode-icons
+code-server --uninstall-extension mhutchie.git-graph
+code-server --uninstall-extension miguelsolorio.fluent-icons
+code-server --uninstall-extension ms-ceintl.vscode-language-pack-zh-hans
+
+code-server --install-extension vscode-icons-team.vscode-icons
+code-server --install-extension wmanth.jar-viewer
